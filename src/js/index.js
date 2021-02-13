2ad7929301c153661273841dc0acbd4ac201e14b
Spinnafre <davispenha@gmail.com>
2021-02-13 00:28:51 -0300

    reaforação

--- src/js/index.js
+++ src/js/index.js
@@ -11,40 +11,107 @@ class ValidarFormulario {
   handleSubmit(e) {
     e.preventDefault();
     const validarCampos = this.validarCampos();
-    alert("OK, ENVIADO COM SUCESSO");
+    const validarSenhas = this.validarSenhas();
+
+    if (validarCampos && validarSenhas) {
+      this.limparCampos();
+      alert("USUÁRIO CADASTRADO COM SUCESSO");
+    }
+  }
+  limparCampos() {
+    let campos = document.querySelectorAll(".validar");
+    campos.forEach((el) => {
+      this.removeMsgError(el.id);
+      el.value = "";
+    });
   }
   validarCampos() {
     let valido = true;
-    const erros = [
-      { msg: "Nome Vazio", name: "name" },
-      { msg: "Sobrenome Vazio", name: "Sobrenome" },
-      { msg: "CPF Vazio", name: "CPF" },
-      { msg: "USUÁRIO Vazio", name: "user" },
-      { msg: "SENHA Vazio", name: "password" },
-      { msg: "CONFIRMAÇÃO DE SENHA Vazio", name: "password-2" },
-    ];
-    let showErrors = [];
     let campos = document.querySelectorAll(".validar");
     campos.forEach((e, i) => {
-      if (!e.value) {
-        showErrors.push(erros[i]);
+      let label=e.previousElementSibling.previousElementSibling.innerHTML
+      if (!e.value && e.id!=='password') {
+        valido = false;
+        this.criarError(`O ${label} não pode estar vazio`,e)
+      }else{
+        this.removeMsgError(e.id)
+      }
+      if (e.id === "user") {
+        if (!this.validarUsuario(e)) valido = false;
+      }
+      if (e.id === "CPF") {
+        if (!this.validarCPF(e)) valido = false;
       }
     });
-    if (showErrors.length) {
-      this.criarErros(showErrors, campos);
-    }
+
+    return valido;
   }
-  criarErros(erros, campos) {
-    campos.forEach((el, i) => {
-      let error = erros.find((err) => el.id === err.name);
-      if (error) {
-        let span = document.querySelector(`#${el.id}`).previousElementSibling;
-        span.innerHTML = error.msg;
-        span.classList.add("error-text");
-      } else {
-        return;
-      }
-    });
+  validarCPF(user) {
+    let valido = true;
+    const cpf = new CPFValidator(user.value);
+    if (!cpf.validade) {
+      valido = false;
+      this.criarError("CPF inválido.", user);
+      return valido;
+    }
+    this.removeMsgError(user.id);
+    return valido;
+  }
+  validarUsuario(user) {
+    let valido = true;
+    if (user.value.length < 3 || user.value.length > 12) {
+      valido = false;
+      this.criarError("Usuário precisa ter entre 3 e 12 caracteres.", user);
+      return valido;
+    }
+    if (!user.value.match(/^[a-zA-Z0-9]+$/g)) {
+      valido = false;
+      this.criarError(
+        "Nome de usuário precisar conter apenas letras e/ou números.",
+        user
+      );
+      return valido;
+    }
+    this.removeMsgError(user.id);
+    return valido;
+  }
+  validarSenhas() {
+    let valido = true;
+    let senha1 = document.getElementById("password");
+    let senha2 = document.getElementById("password-2");
+    if (senha1.value !== senha2.value) {
+      valido = false;
+      this.criarError(
+        "Campos senha e confirmação de senha precisam ser iguais.",
+        senha1
+      );
+      this.criarError(
+        "Campos senha e confirmação de senha precisam ser iguais.",
+        senha2
+      );
+      return valido;
+    }
+    if (senha1.value.length < 6 || senha1.value.length > 12) {
+      valido = false;
+      this.criarError("Senha precisa estar entre 6 e 12 caracteres.", senha1);
+      return valido;
+    }
+    this.removeMsgError(senha1.id);
+    this.removeMsgError(senha2.id);
+    return valido;
+  }
+  criarError(error, campo) {
+    this.showMsgError(campo.id, error);
+  }
+  showMsgError(campo, msg) {
+    let span = document.querySelector(`#${campo}`).previousElementSibling;
+    span.innerHTML=msg
+    span.classList.add("error-text");
+  }
+  removeMsgError(campo) {
+    let span = document.querySelector(`#${campo}`).previousElementSibling;
+    span.innerHTML = "";
+    span.classList.remove("error-text");
   }
 }
 const valid = new ValidarFormulario();
--- src/js/validate.js
+++ src/js/validate.js
@@ -1,46 +1,46 @@
-// class CPFValidator {
-//   constructor(cpf) {
-//     Object.defineProperty(this, "formatCPF", {
-//       writable: false,
-//       enumerable: true,
-//       configurable: false,
-//       value: cpf.replace(/\D+/g, ""),
-//     });
-//   }
-//   get checkSequence() {
-//     if (this.formatCPF.charAt(0).repeat(11) === this.formatCPF) {
-//       return false;
-//     } else {
-//       return true;
-//     }
-//   }
-//   generateNewCPF() {
-//     const newCPF = this.formatCPF.slice(0, -2);
-//     const digito1 = CPFValidator.geraDigito(newCPF);
-//     const digito2 = CPFValidator.geraDigito(newCPF + digito1);
-//     this.NewCPFFormated = newCPF + digito1 + digito2;
-//   }
-//   static geraDigito(cpfSemDigitos) {
-//     let cpfArray = Array.from(cpfSemDigitos);
-//     let reverso = cpfArray.length + 1;
+class CPFValidator {
+  constructor(cpf) {
+    Object.defineProperty(this, "formatCPF", {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpf.replace(/\D+/g, ""),
+    });
+  }
+  get checkSequence() {
+    if (this.formatCPF.charAt(0).repeat(11) === this.formatCPF) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  generateNewCPF() {
+    const newCPF = this.formatCPF.slice(0, -2);
+    const digito1 = CPFValidator.geraDigito(newCPF);
+    const digito2 = CPFValidator.geraDigito(newCPF + digito1);
+    this.NewCPFFormated = newCPF + digito1 + digito2;
+  }
+  static geraDigito(cpfSemDigitos) {
+    let cpfArray = Array.from(cpfSemDigitos);
+    let reverso = cpfArray.length + 1;
 
-//     let total = cpfArray.reduce((ac, val) => {
-//       ac += reverso * Number(val);
-//       reverso--;
-//       return ac;
-//     }, 0);
-//     let result = 11 - (total % 11);
-//     return result > 9 ? "0" : result;
-//   }
-//   get validade() {
-//     if (this.formatCPF.length !== 11) return false;
-//     if (typeof this.formatCPF !== "string") return false;
-//     if (!this.checkSequence) return false;
-//     if (!this.formatCPF) return false;
-//     this.generateNewCPF();
-//     return this.NewCPFFormated === this.formatCPF;
-//   }
-// }
+    let total = cpfArray.reduce((ac, val) => {
+      ac += reverso * Number(val);
+      reverso--;
+      return ac;
+    }, 0);
+    let result = 11 - (total % 11);
+    return result > 9 ? "0" : result;
+  }
+  get validade() {
+    if (this.formatCPF.length !== 11) return false;
+    if (typeof this.formatCPF !== "string") return false;
+    if (!this.checkSequence) return false;
+    if (!this.formatCPF) return false;
+    this.generateNewCPF();
+    return this.NewCPFFormated === this.formatCPF;
+  }
+}
 
 // const cpf = new CPFValidator("070.987.720-03");
 // if (cpf.validade) {
@@ -49,46 +49,46 @@
 //   console.log("CPF inválido");
 // }
 
-function CPFValidator(cpf) {
-  Object.defineProperty(this, "formatCPF", {
-    enumerable: true,
-    get: () => {
-      return cpf.replace(/\D+/g, "");
-    },
-  });
-}
-CPFValidator.prototype.checkSequence = function() {
-  if (this.formatCPF.charAt(0).repeat(11) === this.formatCPF) {
-    return false;
-  } else {
-    return true;
-  }
-};
-CPFValidator.prototype.generateNewCPF = function () {
-  const newCPF = this.formatCPF.slice(0, -2);
-  const digito1 = this.geraDigito(newCPF);
-  const digito2 = this.geraDigito(newCPF + digito1);
-  this.NewCPFFormated = newCPF + digito1 + digito2;
-};
-CPFValidator.prototype.validate = function () {
-  if (this.formatCPF.length !== 11) return false;
-  if (typeof this.formatCPF !== "string") return false;
-  if (!this.checkSequence()) return false;
-  if (!this.formatCPF) return false;
-  this.generateNewCPF();
-  return this.NewCPFFormated === this.formatCPF;
-};
-CPFValidator.prototype.geraDigito = function(cpfSemDigitos) {
-  let cpfArray = Array.from(cpfSemDigitos);
-  let reverso = cpfArray.length + 1;
+// function CPFValidator(cpf) {
+//   Object.defineProperty(this, "formatCPF", {
+//     enumerable: true,
+//     get: () => {
+//       return cpf.replace(/\D+/g, "");
+//     },
+//   });
+// }
+// CPFValidator.prototype.checkSequence = function() {
+//   if (this.formatCPF.charAt(0).repeat(11) === this.formatCPF) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// };
+// CPFValidator.prototype.generateNewCPF = function () {
+//   const newCPF = this.formatCPF.slice(0, -2);
+//   const digito1 = this.geraDigito(newCPF);
+//   const digito2 = this.geraDigito(newCPF + digito1);
+//   this.NewCPFFormated = newCPF + digito1 + digito2;
+// };
+// CPFValidator.prototype.validate = function () {
+//   if (this.formatCPF.length !== 11) return false;
+//   if (typeof this.formatCPF !== "string") return false;
+//   if (!this.checkSequence()) return false;
+//   if (!this.formatCPF) return false;
+//   this.generateNewCPF();
+//   return this.NewCPFFormated === this.formatCPF;
+// };
+// CPFValidator.prototype.geraDigito = function(cpfSemDigitos) {
+//   let cpfArray = Array.from(cpfSemDigitos);
+//   let reverso = cpfArray.length + 1;
 
-  let total = cpfArray.reduce((ac, val) => {
-    ac += reverso * Number(val);
-    reverso--;
-    return ac;
-  }, 0);
-  let result = 11 - (total % 11);
-  return result > 9 ? "0" : result;
-};
+//   let total = cpfArray.reduce((ac, val) => {
+//     ac += reverso * Number(val);
+//     reverso--;
+//     return ac;
+//   }, 0);
+//   let result = 11 - (total % 11);
+//   return result > 9 ? "0" : result;
+// };
 // const p1 = new CPFValidator("070.987.720-03");
 // console.log(p1.validate())
